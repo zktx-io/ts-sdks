b0d3074f4af8665b21795794880c423af3c09868
Michael Hayes <michael.hayes@mystenlabs.com>
2025-01-03 15:29:42 -0800

    Initial copy of sdk packages

---
+++ graphql.config.ts
@@ -0,0 +1,19 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import type { IGraphQLConfig } from 'graphql-config';
+
+const config: IGraphQLConfig = {
+	projects: {
+		tsSDK: {
+			schema: './packages/typescript/src/graphql/generated/latest/schema.graphql',
+			documents: [
+				'./packages/graphql-transport/src/**/*.ts',
+				'./packages/graphql-transport/src/**/*.graphql',
+			],
+			include: ['./packages/graphql-transport/src/**/*.ts', './packages/graphql-transport/src/**/*.graphql'],
+		},
+	},
+};
+
+export default config;
