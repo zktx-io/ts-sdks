3fb0d85249ecbf10a0c7e0c5a3a8b5b8c35cadd1
Michael Hayes <michael.hayes@mystenlabs.com>
2025-01-16 10:26:45 -0800

    Add basic BCS types and utils to walrus sdk

---
+++ packages/walrus/src/utils/bcs.ts
@@ -0,0 +1,48 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { bcs } from '@mysten/bcs';
+
+const MerkleNode = bcs.enum('MerkleNode', {
+	Empty: null,
+	Digest: bcs.bytes(32),
+});
+
+const SliverPairMetadata = bcs.struct('SliverPairMetadata', {
+	primary_hash: MerkleNode,
+	secondary_hash: MerkleNode,
+});
+
+const EncodingType = bcs.enum('EncodingType', {
+	RedStuff: null,
+});
+
+export const BlobMetadataV1 = bcs.struct('BlobMetadataV1', {
+	encoding_type: EncodingType,
+	unencoded_length: bcs.u64(),
+	hashes: bcs.vector(SliverPairMetadata),
+});
+
+export const BlobMetadata = bcs.enum('BlobMetadata', {
+	V1: BlobMetadataV1,
+});
+
+export const BlobMetadataWithId = bcs.struct('BlobMetadataWithId', {
+	blob_id: bcs.bytes(32),
+	metadata: BlobMetadata,
+});
+
+const Symbols = bcs.struct('Symbols', {
+	data: bcs.vector(bcs.u8()),
+	symbol_size: bcs.u16(),
+});
+
+export const SliverData = bcs.struct('SliverData', {
+	symbols: Symbols,
+	index: bcs.u16(),
+});
+
+export const Sliver = bcs.enum('Sliver', {
+	Primary: SliverData,
+	Secondary: SliverData,
+});
